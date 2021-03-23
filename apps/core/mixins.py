a48a4c55530515eb923330de06db156228e3652d
Leslie Chan <leslie_chan@wochacha.com>
2021-03-23 16:29:04 +0800

    UPdate

--- apps/core/mixins.py
+++ apps/core/mixins.py
@@ -4,15 +4,20 @@ from rest_framework_tracking.base_mixins import BaseLoggingMixin
 
 class LogRequestMixin(BaseLoggingMixin):
     need_log = False
+    logging_actions = []
 
     def should_log(self, request, response):
         """
         Method that should return a value that evaluated to True if the request should be logged.
         By default, check if the request method is in logging_methods.
         """
-        if self.need_log:
+        if self.need_log or self.action in self.logging_actions:
             return True
         return False
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        response = super(LogRequestMixin, self).finalize_response(request, response, *args, **kwargs)
+        return response
 
     def handle_log(self):
         """
--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,16 @@ from django.conf.urls import url
 from django.conf import settings
 from django.views.generic import TemplateView
 
+from apps.test_model.views import WatchGtViewSet
+
+
 apipatterns = [
     path("main/", include('apps.test_model.urls', namespace="test")),  # test模块
     path("user/", include('apps.user.urls', namespace="user"))  # 用户模块
 ]
 
 urlpatterns = [
+    path('Watch/', WatchGtViewSet.as_view(actions={'get': 'list'}), name="Watch"),
     path('admin/', admin.site.urls),
     # path(r"_nested_admin/", include("nested_admin.urls")),
     path('api/', include(apipatterns)),
